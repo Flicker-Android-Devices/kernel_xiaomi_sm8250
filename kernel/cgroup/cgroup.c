21349e212bea95d642b64acd2fda2a9dbc5f91d6
Christian Brauner <christian.brauner@ubuntu.com>
2020-02-05 14:26:18 +0100

    BACKPORT: cgroup: unify attach permission checking
    The core codepaths to check whether a process can be attached to a
    cgroup are the same for threads and thread-group leaders. Only a small
    piece of code verifying that source and destination cgroup are in the
    same domain differentiates the thread permission checking from
    thread-group leader permission checking.
    Since cgroup_migrate_vet_dst() only matters cgroup2 - it is a noop on
    cgroup1 - we can move it out of cgroup_attach_task().
    All checks can now be consolidated into a new helper
    cgroup_attach_permissions() callable from both cgroup_procs_write() and
    cgroup_threads_write().
    
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Li Zefan <lizefan@huawei.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: cgroups@vger.kernel.org
    Acked-by: Michal Koutný <mkoutny@suse.com>
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Flicker372 <flicker372@outlook.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -2800,11 +2800,8 @@ int cgroup_attach_task(struct cgroup *dst_cgrp, struct task_struct *leader,
 {
 	DEFINE_CGROUP_MGCTX(mgctx);
 	struct task_struct *task;
-	int ret;
 
-	ret = cgroup_migrate_vet_dst(dst_cgrp);
-	if (ret)
-		return ret;
+	int ret = 0;
 
 	/* look up all src csets */
 	spin_lock_irq(&css_set_lock);
@@ -4809,6 +4806,27 @@ static int cgroup_procs_write_permission(struct cgroup *src_cgrp,
 	return 0;
 }
 
+static int cgroup_attach_permissions(struct cgroup *src_cgrp,
+				     struct cgroup *dst_cgrp,
+				     struct super_block *sb, bool threadgroup,
+				     struct cgroup_namespace *ns)
+{
+	int ret = 0;
+
+	ret = cgroup_procs_write_permission(src_cgrp, dst_cgrp, sb, ns);
+	if (ret)
+		return ret;
+
+	ret = cgroup_migrate_vet_dst(dst_cgrp);
+	if (ret)
+		return ret;
+
+	if (!threadgroup && (src_cgrp->dom_cgrp != dst_cgrp->dom_cgrp))
+		ret = -EOPNOTSUPP;
+
+	return ret;
+}
+
 static ssize_t cgroup_procs_write(struct kernfs_open_file *of,
 				  char *buf, size_t nbytes, loff_t off)
 {
@@ -4838,9 +4856,9 @@ static ssize_t cgroup_procs_write(struct kernfs_open_file *of,
 	 * inherited fd attacks.
 	 */
 	saved_cred = override_creds(of->file->f_cred);
-	ret = cgroup_procs_write_permission(src_cgrp, dst_cgrp,
-					    of->file->f_path.dentry->d_sb,
-					    ctx->ns);
+	ret = cgroup_attach_permissions(src_cgrp, dst_cgrp,
+					of->file->f_path.dentry->d_sb,
+					true, ctx->ns);
 	revert_creds(saved_cred);
 	if (ret)
 		goto out_finish;
