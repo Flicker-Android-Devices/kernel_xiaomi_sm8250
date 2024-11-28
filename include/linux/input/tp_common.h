816c1f9282ee9e221f8d83fa87c46b30b1ecb20d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-11-28 23:58:37 +0800

    drivers: input: touchscreen: tp_common: Add pen node
    Change-Id: Ie48813c82de127f60b524fdeb8d1010ee0dbfbd4

--- include/linux/input/tp_common.h
+++ include/linux/input/tp_common.h
@@ -16,3 +16,4 @@ int tp_common_set_double_tap_ops(struct tp_common_ops *ops);
 int tp_common_set_fod_status_ops(struct tp_common_ops *ops);
 int tp_common_set_fp_state_ops(struct tp_common_ops *ops);
 int tp_common_set_reversed_keys_ops(struct tp_common_ops *ops);
+int tp_common_set_pen_ops(struct tp_common_ops *ops);
