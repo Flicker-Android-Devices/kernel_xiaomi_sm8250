5fd3aae7136e13694a519727126aa7a33c329da3
Flicker372 <flicker372@outlook.com>
2025-01-10 01:12:59 +0800

    techpack: audio: Import missing Xiaomi changes
    * From alioth-r-oss
    
    Co-authored-by: Alcatraz323 <alcatraz32323@gmail.com>
    Signed-off-by: Flicker372 <flicker372@outlook.com>

--- techpack/audio/asoc/codecs/cs35l41/wm_adsp.c
+++ techpack/audio/asoc/codecs/cs35l41/wm_adsp.c
@@ -4420,7 +4420,7 @@ static int wm_halo_apply_calibration(struct snd_soc_dapm_widget *w)
 				wm_adsp_k_ctl_get(dsp, "RCV DSP1X Protection cd CAL_STATUS");
 				wm_adsp_k_ctl_get(dsp, "RCV DSP1X Protection cd CAL_CHECKSUM");
 				//for ultrasonic
-#if defined(CONFIG_TARGET_PRODUCT_APOLLO) || defined(CONFIG_TARGET_PRODUCT_CAS) || defined (CONFIG_TARGET_PRODUCT_ALIOTH) || defined (CONFIG_TARGET_PRODUCT_PSYCHE)
+#if defined(CONFIG_TARGET_PRODUCT_APOLLO) || defined(CONFIG_TARGET_PRODUCT_CAS) || defined (CONFIG_TARGET_PRODUCT_ALIOTH) || defined (CONFIG_TARGET_PRODUCT_PSYCHE) || defined (CONFIG_TARGET_PRODUCT_THYME)
 				wm_adsp_k_ctl_put(dsp, "RCV DSP1X Protection 400a4 E_FULL_US_BYPASS", 1);
 				wm_adsp_k_ctl_get(dsp, "RCV DSP1X Protection 400a4 E_FULL_US_BYPASS");
 #endif
