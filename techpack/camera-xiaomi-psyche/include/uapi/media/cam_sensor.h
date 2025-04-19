e482d5851472ab2d16cee0812d958ffc599c5a84
Flicker372 <flicker372@outlook.com>
2025-04-19 21:11:35 +0800

    techpack: camera-xiaomi-psyche: Implement EIS/OIS handling
    [lamjiidii1][Moonlight4004]:
     - Fix OIS firmware not loading correctly
     - Improve camera ois handling based on Xiaomi 13 Pro 5G
    
    [Flicker372]:
    - Based on psyche-r-oss/techpack/camera-dxo
    
    Co-authored-by: lamjiidii1 <lamjiidii1@gmail.com>
    Co-authored-by: Moonlight4004 <87663594+Moonlight4004@users.noreply.github.com>
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- techpack/camera-xiaomi-psyche/include/uapi/media/cam_sensor.h
+++ techpack/camera-xiaomi-psyche/include/uapi/media/cam_sensor.h
@@ -123,8 +123,8 @@ struct cam_ois_opcode {
 	uint8_t  fw_addr_type; //Xiaomi add
 	uint8_t  is_addr_increase; //Xiaomi add
 	uint8_t  is_addr_indata; //Xiaomi add
-        uint8_t  fwversion;
-        uint32_t fwchecksumsize;
+    uint8_t  fwversion;
+    uint32_t fwchecksumsize;
 	uint32_t fwchecksum;
 } __attribute__((packed));
 
@@ -137,6 +137,7 @@ struct cam_ois_opcode {
  * @ois_fw_flag           :    indicates if fw is present or not
  * @is_ois_calib          :    indicates the calibration data is available
  * @is_ois_pre_init       :    indicates the pre initialize data is available
+ * @is_ois_post_init      :    indicates the post initialize data is available 
  * @ois_name              :    OIS name
  * @opcode                :    opcode
  */
@@ -147,6 +148,7 @@ struct cam_cmd_ois_info {
 	uint8_t               ois_fw_flag;
 	uint8_t               is_ois_calib;
 	uint8_t               is_ois_pre_init; //xiaomi add
+	uint8_t               is_ois_post_init;	
 	char                  ois_name[MAX_OIS_NAME_SIZE];
 	struct cam_ois_opcode opcode;
 } __attribute__((packed));
