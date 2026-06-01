5489b7b79b587efea0b57f8f1b59d6f1b4165c9c
Flicker372 <flicker372@outlook.com>
2026-06-01 23:37:48 +0800

    fixup! mm: Convert add_to_swap_cache to XArray

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -413,9 +413,8 @@ extern struct address_space *swapper_spaces[];
 extern unsigned long total_swapcache_pages(void);
 extern void show_swap_cache_info(void);
 extern int add_to_swap(struct page *page);
-extern int add_to_swap_cache(struct page *, swp_entry_t, gfp_t);
-extern int __add_to_swap_cache(struct page *page, swp_entry_t entry,
-			void **shadowp);
+extern int add_to_swap_cache(struct page *page, swp_entry_t entry,
+			gfp_t gfp, void **shadowp);
 extern void __delete_from_swap_cache(struct page *page, void *shadow);
 extern void delete_from_swap_cache(struct page *);
 extern void clear_shadow_from_swap_cache(int type, unsigned long begin,
