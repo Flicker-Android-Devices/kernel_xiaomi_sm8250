9d509f061dfdf37da04b04369ddf38bb99441378
Flicker372 <flicker372@outlook.com>
2026-09-14 15:51:14 +0800

    techpack: camera: Adapt CSID reset and OIS opcode ABI for CONFIG_CAMERA_CAS
    Signed-off-by: Flicker372 <flicker372@outlook.com>

--- techpack/camera/drivers/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_core.c
+++ techpack/camera/drivers/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_core.c
@@ -694,7 +694,7 @@ static int cam_ife_csid_global_reset(struct cam_ife_csid_hw *csid_hw)
 		cam_io_w_mb(0x2, soc_info->reg_map[0].mem_base +
 			csid_reg->rdi_reg[i]->csid_rdi_cfg0_addr);
 
-#ifdef CONFIG_MACH_XIAOMI_PSYCHE
+#ifdef CONFIG_CAMERA_CAS
 	/* reset SW regs first, then HW */
 	rc = cam_ife_csid_reset_regs(csid_hw, false);
 	if (rc < 0)
